311a4bba2504c9075809118c3b098d429e996fa1
benjamin cadars <benjamin.cadars@gmail.com>
2023-09-19 14:45:34 +0200

    Resto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  #verb path controller#action
-  get 'contact', to: 'pages#contact'
+  # root to: 'pages#home'
+  # get 'about', to: 'pages#about'
+  # #verb path controller#action
+  # get 'contact', to: 'pages#contact'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
